e380f8cb090babdf9ba820b47b488635503a781b
Juan <jm-barreto1@hotmail.com>
2021-04-21 22:31:12 -0400

    Wrote LoadData() to fill the vectorlist

--- DM System/src/DMS.cpp
+++ DM System/src/DMS.cpp
@@ -1,5 +1,7 @@
 #include "DMS.h"
+#include <fstream>
 #include <algorithm>
+#include <string>
 
 void dms::DMS::addContact(Contact& contact)
 {
@@ -27,4 +29,36 @@ void dms::DMS::registerQuery(string& key, Query* const query_func)
 void dms::DMS::loadData(const string& filepath)
 {
 	// TODO: Populate the DMS
+	vector <string> tokenizedfile;
+	int size;	
+	string first;
+	string type;
+	ifstream file;
+	file.open(filepath, ios::in);
+	getline(file, first, ',');
+	size = stoi(first.substr(0, first.find(",") + 1));
+	type = first.substr(first.rfind(",") + 1);
+	contacts.resize(size);
+	
+	if (type == "person") {
+		if (file.is_open()) {
+			string person;
+			while (getline(file, person,' ')) {
+				tokenizedfile.push_back(person);
+				Contact* new_phone = &PersonPhoneContact(tokenizedfile[0], tokenizedfile[1], tokenizedfile[2]);
+				Contact* new_email = &PersonEmailContact(tokenizedfile[0], tokenizedfile[1], tokenizedfile[3]);
+				Contact* new_address = &PersonAddressContact(tokenizedfile[0], tokenizedfile[1], tokenizedfile[4], tokenizedfile[5], tokenizedfile[6], tokenizedfile[7]);
+				contacts.push_back(new_phone);
+				contacts.push_back(new_email);
+				contacts.push_back(new_address);
+			}
+		}
+
+	}
+	else if("business"){
+		//TODO: business contact generation using input file 2
+	}
+	else {
+		throw uncaught_exceptions;
+	}
 }
