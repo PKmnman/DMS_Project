29a533310974691d3b3f6e30f2edb33429c6578e
PKmnman <gareeves@my.bridgeport.edu>
2021-05-06 16:08:51 -0400

    Removed call to getQuery. It doesn't exist

--- DM System/src/headers/Partition.h
+++ DM System/src/headers/Partition.h
@@ -23,10 +23,12 @@ namespace dms
 			this->server_num = server_num;
 			this->query = query;
 		}
+		
 		string getPartition() { 
-			cout << server_num << query->getQuery() << endl;
+			cout << server_num << query << endl;
 		
 		}
+		
 		void getServer();
 		string getQuery() { cout << query << endl; }
 		void ServerPing(int server_num);
@@ -43,6 +45,7 @@ namespace dms
 	
 	public:
 		int current_slot;
+		
 		TimingWheel(int size)
 		{
 			server = new Partition*[size];
@@ -50,9 +53,11 @@ namespace dms
 				server[i] = nullptr;
 			}
 		}
+		
 		void insert(int processing_time, int server_num, Partition* p);
 		void schedule();
 		void clear_curr_slot(int current_slot);
+		
 		size_t nextIndex(int current_slot, int server_size);
 		size_t ServerPing(int current_slot);
 	};
