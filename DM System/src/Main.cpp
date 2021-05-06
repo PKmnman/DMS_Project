7badf1d8b9357201650c4e106d376ed3b4afc251
Juan <jm-barreto1@hotmail.com>
2021-05-06 12:21:21 -0400

    This commit has the menu i wrote last night that go overwritten by the last commit

--- DM System/src/Main.cpp
+++ DM System/src/Main.cpp
@@ -1,16 +1,48 @@
 #include "Contact.h"
 #include "DMS.h"
 #include "Partition.h"
-#include <fstream>
+#include "Menu.h"
+#include <iostream>
 
+using namespace std;
 using namespace dms;
-using namespace dms::contact;
+using namespace menu;
 
 int main()
 {
+	Menu menu;
 	DMS dms;
-	dms = DMS::getDMS();
-	//DMS::getDMS();
+	std::string input;
+	menu.setTitle(":Main Menu:");
+	cout << "please select an option:" << endl;
+	menu.addOption("startDMS", load);
+	menu.addOption("searchQuery", search);
+	menu.addOption("displayQuery", display);
+	menu.addOption("Partition/TimingWheel", partition);
+	cin >> input;
+	menu.select(input);
+
+
+}
+
+void load() {
+	cout << "Starting DMS.." << endl;;
+	DMS dms;
+	dms.loadData("input.txt");
+	cout << "DMS Loaded From File" << endl;
+	main();
+}
+void search() {
+	DMS dms;
+	string in;
+	dms.loadData("input.txt");
+	cout << "Please input the name of your Search Query:" << endl;
+	cin >> in;
+	dms.getQuery(in);
+	
+}
+void display() {
+	DMS dms;
 	dms.loadData("input.txt");
 	DisplayQuery display("john");
 }
