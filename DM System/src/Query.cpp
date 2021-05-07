6184570507a981ff3e68b3f23fe699d3958baed5
PKmnman <gareeves@my.bridgeport.edu>
2021-05-07 12:35:29 -0400

    Tweaked display

--- DM System/src/Query.cpp
+++ DM System/src/Query.cpp
@@ -71,8 +71,8 @@ namespace dms
 			{
 				const auto info = dynamic_cast<AddressInfo*>(c);
 				cout << '\t' << setw(WIDTH) << left
-					<< "Address: " << info->getStreetAddress() << "\n"
-					<< setw(WIDTH) << left << '\t' << info->getDistrict() << ", "
+					<< "Address: " << info->getStreetAddress() << "\n\t"
+					<< setw(WIDTH) << left  << info->getDistrict() << ", "
 					<< info->getState() << " " << info->getZipcode()
 					<< endl;
 			}
